41e8b13f2264864f620ab8321c64ee1859f7fb74
Breno Pedro <brenopedro14@hotmail.com>
2022-03-23 22:00:36 -0300

    Create cube battery voltage controller and pages

---
+++ src/main/java/com/reservoir/datareservoir/client/controller/cube/CubeBatteryVoltageController.java
@@ -0,0 +1,50 @@
+package com.reservoir.datareservoir.client.controller.cube;
+
+import com.reservoir.datareservoir.client.domain.model.PropertiesFilter;
+import com.reservoir.datareservoir.client.domain.service.CubeDataService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/cube/battery-voltage")
+public class CubeBatteryVoltageController {
+
+    @GetMapping
+    public ModelAndView cubeBatteryCurrent() {
+        return new ModelAndView("cube/tabs/batteryVoltage/batteryVoltage");
+    }
+
+    @GetMapping("/table")
+    public ModelAndView getCubeBatteryCurrentTable(PropertiesFilter propertiesFilter) {
+        ModelAndView modelAndView = new ModelAndView("cube/tabs/batteryVoltage/table");
+        modelAndView.addObject("cubeData", CubeDataService.getCubeData(propertiesFilter));
+
+        return modelAndView;
+    }
+
+    @PostMapping("/table")
+    public ModelAndView postCubeBatteryCurrentTable(PropertiesFilter propertiesFilter) {
+        ModelAndView modelAndView = new ModelAndView("cube/tabs/batteryVoltage/table");
+        modelAndView.addObject("cubeData", CubeDataService.getCubeData(propertiesFilter));
+
+        return modelAndView;
+    }
+
+    @GetMapping("/graph")
+    public ModelAndView getCubeBatteryCurrentGraph(PropertiesFilter propertiesFilter) {
+        ModelAndView modelAndView = new ModelAndView("cube/tabs/batteryVoltage/graph");
+        modelAndView.addObject("cubeData", CubeDataService.getCubeData(propertiesFilter));
+
+        return modelAndView;
+    }
+
+    @PostMapping("/graph")
+    public ModelAndView postCubeBatteryCurrentGraph(PropertiesFilter propertiesFilter) {
+        ModelAndView modelAndView = new ModelAndView("cube/tabs/batteryVoltage/graph");
+        modelAndView.addObject("cubeData", CubeDataService.getCubeData(propertiesFilter));
+        return modelAndView;
+    }
+}
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,3 +1,5 @@
 $('#button').click(function () {
     location.reload()
 });
+
+setTimeout(() => {location.reload()}, 60000);
